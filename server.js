2589139db92fca88cc3484ee9a496225e3079208
Ivan Duranic <ivan.duranic@gmail.com>
2021-03-15 23:32:34 -0400

    deploying

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/GoogleBooks";
 
 mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.listen(PORT, () => {
